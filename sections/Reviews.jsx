f1b1dc28b3390eb794011b410d4f0ae7e92a13b6
Bleyle <Bennybleylle09@gmail.com>
2024-03-16 13:58:41 +0300

    Add files via upload

--- sections/Reviews.jsx
+++ sections/Reviews.jsx
@@ -8,7 +8,7 @@ const Reviews = () => (
       <div class="flex justify-center flex-wrap -m-4">
         <div class="lg:w-1/3 lg:mb-0 mb-6 p-4">
           <div class="h-full text-center">
-            <img alt="testimonial" class="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://dummyimage.com/302x302"/> 
+            <img alt="testimonial" class="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="/Logos/KENINDIA-LOGO-1.png"/> 
             {/* <span class="inline-block h-1 w-10 rounded bg-purple-500 mt-6 mb-4"></span> */}
             <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">Kenindia Assurance company</h2>
             <p class="text-gray-500"></p>
@@ -16,7 +16,7 @@ const Reviews = () => (
         </div>
         <div class="lg:w-1/3 lg:mb-0 mb-6 p-4">
           <div class="h-full text-center">
-            <img alt="testimonial" class="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://dummyimage.com/302x302"/> 
+            <img alt="testimonial" class="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="/Logos/Geminia-logo.png"/> 
             {/* <span class="inline-block h-1 w-10 rounded bg-purple-500 mt-6 mb-4"></span> */}
             <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">Geminia motor insurance company </h2>
             <p class="text-gray-500"></p>
@@ -35,7 +35,7 @@ const Reviews = () => (
       <div class="flex justify-center flex-wrap -m-4 mt-10">
         <div class="lg:w-1/3 lg:mb-0 mb-6 p-4">
           <div class="h-full text-center">
-            <img alt="testimonial" class="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://dummyimage.com/302x302"/> 
+            <img alt="testimonial" class="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="/Logos/hopecarelogox.png"/> 
             {/* <span class="inline-block h-1 w-10 rounded bg-purple-500 mt-6 mb-4"></span> */}
             <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">Hope care hospital
 </h2>
@@ -44,7 +44,7 @@ const Reviews = () => (
         </div>
         <div class="lg:w-1/3 lg:mb-0 mb-6 p-4">
           <div class="h-full text-center">
-            <img alt="testimonial" class="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://dummyimage.com/302x302"/> 
+            <img alt="testimonial" class="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="/Logos/Xplico-logo.png"/> 
             {/* <span class="inline-block h-1 w-10 rounded bg-purple-500 mt-6 mb-4"></span> */}
             <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">Xplico insurance company 
 </h2>
@@ -53,7 +53,7 @@ const Reviews = () => (
         </div>
         <div class="lg:w-1/3 lg:mb-0 mb-6 p-4">
           <div class="h-full text-center">
-            <img alt="testimonial" class="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://dummyimage.com/302x302"/> 
+            <img alt="Kivaa" class="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="/Logos/Kivaa-logo.png"/> 
             {/* <span class="inline-block h-1 w-10 rounded bg-purple-500 mt-6 mb-4"></span> */}
             <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">Kivaa secondary school</h2>
             <p class="text-gray-500"></p>
@@ -62,18 +62,10 @@ const Reviews = () => (
       </div>
 
       <div class="flex justify-center flex-wrap -m-4 mt-10">
+       
         <div class="lg:w-1/3 lg:mb-0 mb-6 p-4">
           <div class="h-full text-center">
-            <img alt="testimonial" class="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://dummyimage.com/302x302"/> 
-            {/* <span class="inline-block h-1 w-10 rounded bg-purple-500 mt-6 mb-4"></span> */}
-            <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">Counterstrike ltd
-</h2>
-            <p class="text-gray-500"></p>
-          </div>
-        </div>
-        <div class="lg:w-1/3 lg:mb-0 mb-6 p-4">
-          <div class="h-full text-center">
-            <img alt="testimonial" class="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://dummyimage.com/302x302"/> 
+            <img alt="testimonial" class="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="/Logos/serenakigali-logo.svg"/> 
             {/* <span class="inline-block h-1 w-10 rounded bg-purple-500 mt-6 mb-4"></span> */}
             <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">Serena Hotel Kigali</h2>
             <p class="text-gray-500"></p>
@@ -88,6 +80,50 @@ const Reviews = () => (
           </div>
         </div>
       </div>
+      <div class="flex justify-center flex-wrap -m-4 mt-10">
+       
+       <div class="lg:w-1/3 lg:mb-0 mb-6 p-4">
+       <div class="flex justify-center flex-wrap -m-4 mt-10">
+       
+       
+       <div class="lg:w-1/3 lg:mb-0 mb-6 p-4">
+         <div class="h-full text-center">
+           <img alt="testimonial" class="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://dummyimage.com/302x302"/> 
+           {/* <span class="inline-block h-1 w-10 rounded bg-purple-500 mt-6 mb-4"></span> */}
+           <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">Mubia Group ltd </h2>
+           <p class="text-gray-500"></p>
+         </div>
+       </div>
+     </div>
+       </div>
+       <div class="lg:w-1/3 lg:mb-0 mb-6 p-4">
+         <div class="h-full text-center">
+           <img alt="testimonial" class="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://dummyimage.com/302x302"/> 
+           {/* <span class="inline-block h-1 w-10 rounded bg-purple-500 mt-6 mb-4"></span> */}
+           <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">Southern Eastern University </h2>
+           <p class="text-gray-500"></p>
+         </div>
+       </div>
+     </div>
+     <div class="flex justify-center flex-wrap -m-4 mt-10">
+       
+       <div class="lg:w-1/3 lg:mb-0 mb-6 p-4">
+       <div class="flex justify-center flex-wrap -m-4 mt-10">
+       
+       
+       <div class="lg:w-1/3 lg:mb-0 mb-6 p-4">
+         <div class="h-full text-center">
+           <img alt="testimonial" class="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://dummyimage.com/302x302"/> 
+           {/* <span class="inline-block h-1 w-10 rounded bg-purple-500 mt-6 mb-4"></span> */}
+           <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">Counterstrike ltd </h2>
+           <p class="text-gray-500"></p>
+         </div>
+       </div>
+     </div>
+       </div>
+       
+     </div>
+
     </div>
   </section></>
   
